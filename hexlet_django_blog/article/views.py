993edd5b2824faacc2d3c1b1ca0f81fbfe88ffb1
nerodnoy <thatsforsure@mail.ru>
2024-05-18 14:52:19 +0300

    added a couple of templates

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('article')
+    name = 'Main page for article'
+    return render(request, 'article/index.html', context={'name': name})
--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -20,6 +20,6 @@ from hexlet_django_blog import views
 
 urlpatterns = [
     path('', views.index),
-    path('articles/', include('hexlet_django_blog.article.urls')),
+    path('article/', include('hexlet_django_blog.article.urls')),
     path('admin/', admin.site.urls),
 ]
