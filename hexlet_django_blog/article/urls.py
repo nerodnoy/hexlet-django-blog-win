1c06e75504276653634f64f88bb3e3d53f654bdd
nerodnoy <thatsforsure@mail.ru>
2024-05-22 13:06:39 +0300

    added a page for each article

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,7 +1,9 @@
-from django.shortcuts import redirect
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView
+from hexlet_django_blog.article.views import IndexView, ArticleView
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('<int:id>/', ArticleView.as_view()),
+    # Для комментариев к статье
+    # path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from hexlet_django_blog.article.models import Article
@@ -11,3 +11,20 @@ class IndexView(View):
         return render(request, 'article/index.html', context={
             'articles': articles,
         })
+
+
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'article/show.html', context={
+            'article': article,
+        })
+
+# Для комментариев к статье
+# class ArticleCommentsView(View):
+#
+#     def get(self, request, *args, **kwargs):
+#         comment = get_object_or_404(Comment, id=kwargs['id'], article__id=kwargs['article_id'])
+#
+#         return render( ... )
