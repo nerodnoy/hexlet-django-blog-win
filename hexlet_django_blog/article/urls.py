f8a3b8541d81a44091393c5b7531e38080e63ad9
nerodnoy <thatsforsure@mail.ru>
2024-07-09 12:08:32 +0300

    added ability to edit articles

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleCommentFormView, ArticleFormCreateView
+from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleCommentFormView, ArticleFormCreateView, \
+    ArticleFormEditView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='articles'),
     path('<int:id>/', ArticleView.as_view()),
     path('<int:article_id>/comments/<int:id>/', ArticleCommentFormView.as_view(), name='comment_create'),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
+    path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
 ]
